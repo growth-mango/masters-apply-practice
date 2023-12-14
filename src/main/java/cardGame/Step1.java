5e3a0e47d31e85fe3098fd17ad93aee56fe555e2
mango <esudatcom@gmail.com>
2023-12-15 00:20:33 +0900

    feat: 생성된 카드를 3*6 형태로 18장 출력하기

--- src/main/java/cardGame/Step1.java
+++ src/main/java/cardGame/Step1.java
@@ -1,18 +1,22 @@
 package cardGame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Step1 {
     public static void main(String[] args) {
+        List<Integer> cards = generateNumbers();
+
         System.out.println(generateNumbers());
         System.out.println(generateNumbers().size());
+        System.out.println(Arrays.deepToString(printGrid(cards)));
 
     }
 
     // 1부터 8까지 각 세 장씩 랜덤하게 들어있는 카드 생성하기
-    public static List<Integer> generateNumbers(){
+    public static List<Integer> generateNumbers() {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= 8; i++) {
             numbers.add(i);
@@ -23,6 +27,19 @@ public class Step1 {
         return numbers;
     }
 
+    // 생성된 카드를 3 * 6 형태로 18장 출력하기
+    public static int[][] printGrid(List<Integer> numbers) {
+        int[][] grid = new int[3][6];
+        int index = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 6; j++) {
+                grid[i][j] = numbers.get(index);
+                index++;
+            }
+        }
+        return grid;
+    }
 
 
 }

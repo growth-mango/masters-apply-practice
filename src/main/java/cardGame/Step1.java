0db0289e7347a1437678938cddfa9e4d5d757b21
mango <esudatcom@gmail.com>
2023-12-18 18:41:42 +0900

    feat: 사용자에게 입력받은 좌표의 카드 뒤집어 보여주는 메서드 구현

--- src/main/java/cardGame/Step1.java
+++ src/main/java/cardGame/Step1.java
@@ -10,6 +10,8 @@ public class Step1 {
         System.out.println(generateNumbers().size());
         System.out.println(Arrays.deepToString(printGrid(cards)));
         printInitialGrid();
+        int[][] inputs = getUserInput();
+        revealCards(printGrid(cards),inputs);
 
     }
 
@@ -52,17 +54,19 @@ public class Step1 {
     // 사용자에게 좌표 입력받기
     public static int[][] getUserInput() {
         Scanner scanner = new Scanner(System.in);
-        int[][] inputs = new int[2][2]; // 좌표 크기 지정
+        int[][] inputs = new int[2][2];
+
+        System.out.println("좌표를 입력하세요.");
 
         for (int i = 0; i < 2; i++) {
-            while (true) { // true 면 계속 실행
-                System.out.print("입력 " + (i + 1) + "? ");
+            while (true) {
+                System.out.print("입력 " + (i + 1) + "? "); // 입력 1 ?
                 String input = scanner.nextLine();
 
-                int[] coordinates = validateInput(input); // 입력 유효성 검증 메소드 구현 해야함
-                if (coordinates != null) {
+                int[] coordinates = validateInput(input); // 입력받은 좌표의 유효성 검증
+                if (coordinates != null) { // 입력받은 좌표가 null 이 아니면 -
                     inputs[i] = coordinates;
-                    break; // 가장 가까운 반복문 (즉 이 코드에서는 while문) 종료 시킴
+                    break; // 가장 가까운 반복문 빠져나감 // 여기서는 while 문
                 }
             }
         }
@@ -71,27 +75,35 @@ public class Step1 {
 
     // 사용자에게 입력받은 좌표 유효성 검증하기
     public static int[] validateInput(String input) {
-        try { // 예외가 발생할 수 있는 코드
-            input = input.toString().replaceAll("[^0-9,]", ""); // 숫자와 쉼표 제외하고(즉 괄호와 공백은) 제거하기
-            String[] parts = input.split(","); // 골호 공백 제거한 문자열을 배열로 만들기
+        try {
+            input = input.toString().replaceAll("[^0-9,]", ""); // 괄호와 공백 없애기 // 숫자와 쉼표만 남음
+            String[] parts = input.split(","); // 쉼표로 구분된 문자열 배열로 저장
 
-            if (parts.length == 2) { // 배열의 길이가 두개가 맞으면
-                // 이부분에서 사용자가 숫자를 입력하지 않았으면 catch 문으로 이동!
-                int row = Integer.parseInt(parts[0]); // 숫자 한 개씩 범위 확인 row 3 이내
-                int col = Integer.parseInt(parts[1]); // col 6 이내
+            if (parts.length == 2) {
+                int row = Integer.parseInt(parts[0]);
+                int col = Integer.parseInt(parts[1]);
 
                 if (row >= 1 && row <= 3 && col >= 1 && col <= 6) {
                     return new int[]{row, col};
-                } else { // 좌표 내에 있지 않다면
-                    System.out.println("잘못된 입력 입니다. 범위 내의 좌표를 숫자로 입력하세요.");
+                } else {
+                    System.out.println("잘못된 입력입니다. 범위 내의 좌표를 입력해주세요.");
                 }
-            } else { // 길이가 2개가 아니면
-                System.out.println("잘못된 입력 입니다. (행, 열) 형태로 숫자를 입력해주세요.");
+            } else {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
             }
-        } catch (NumberFormatException e) { // 예외가 발생할 경우 어떻게 대처할지. try 내의 코드에서 예외가 발생하면
+        } catch (NumberFormatException e) {
             System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
         }
         return null;
+    }
+
+    // 사용자에게 입력받은 좌표의 카드를 뒤집어서 보여주기
+    public static void revealCards(int[][] grid, int[][] inputs) {
+        for(int[] input : inputs){
+            int row = input[0] -1 ; // 입력은 1부터, 인덱스는 0부터 시작하니까 조정
+            int col = input[1] -1 ;
+            System.out.println(grid[row][col]);
+        }
     }
 
 }
